7410fa19195a2f960eac8dbe4ec8ffe4466db7f5
Bernard Chisumo <bchisumo74@yahoo.com>
2017-04-30 13:12:52 +0100

    Set a migration for pages , services , portifoio,Client including the controller for each pages . IndexController , PageController, PagesController, ServicesController, PortifoliosController

---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware'=>'web'], function(){
+    Route::match(['get','post'], '/', ['uses'=>'IndexController@execute','as'=>'home']);
+    Route::get('/pages/{alias}',['uses'=>'PageController@execute', 'as'=>'page']);
+
+    Route::auth();
+});
+
+//admin/service
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function(){
+
+    //admin
+    Route::get('/',function(){
+
+    });
+
+    //admin/pages
+    Route::group(['prefix'=>'pages'], function(){
+
+        //admin/pages
+        Route::get('/', ['uses'=>'PagesControler@execute','as'=>'pages']);
+
+        //admin/pages/add
+        Route::match(['get','post'], '/add', ['uses'=>'PagesAddController@execute','as'=>'pagesAdd']);
+        //admin/pages/edit
+        Route::match(['get','post', 'delete'], '/edit/{page}', ['uses'=>'PagesEditController@execute', 'as'=>'PagesEdit']);
+
+
+    });
+
+
+    //admin/portfolios
+    Route::group(['prefix'=>'portfolios'], function(){
+
+        //admin/portfolios
+        Route::get('/', ['uses'=>'portfoliosControler@execute','as'=>'portfolios']);
+
+        //admin/portfolios/add
+        Route::match(['get','post'], '/add', ['uses'=>'portfoliosAddController@execute','as'=>'portfoliosAdd']);
+        //admin/portfolios/edit
+        Route::match(['get','post', 'delete'], '/edit/{portfolios}', ['uses'=>'portfoliosEditController@execute', 'as'=>'portfoliosEdit']);
+
+
+    });
+
+
+    //admin/portfolios
+    Route::group(['prefix'=>'services'], function(){
+
+        //admin/services
+        Route::get('/', ['uses'=>'servicesControler@execute','as'=>'services']);
+
+        //admin/services/add
+        Route::match(['get','post'], '/add', ['uses'=>'servicesAddController@execute','as'=>'servicessAdd']);
+        //admin/services/edit
+        Route::match(['get','post', 'delete'], '/edit/{services}', ['uses'=>'portfoliosEditController@execute', 'as'=>'servicesEdit']);
+
+
+    });
+});
